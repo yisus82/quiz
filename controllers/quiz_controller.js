1a1f27d23b6c1a178244e4ff6dcca2fa3cdb104b
Jesús Ángel Pérez-Roca Fernández <djalma8@gmail.com>
2015-07-28 23:33:39 +0200

    Depliegue DB en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,13 @@
 // GET /quizes/question
 exports.question = function(req, res) {
-   models.Quiz.findAll().success(function(quiz) {
+   models.Quiz.findAll().then(function(quiz) {
 	res.render('quizzes/question', { pregunta: quiz[0].pregunta});
   })
 };
 
 // GET /quizes/answer
 exports.answer = function(req, res) {
-   models.Quiz.findAll().success(function(quiz) {
+   models.Quiz.findAll().then(function(quiz) {
     if (req.query.respuesta === quiz[0].respuesta) {
       res.render('quizzes/answer', { respuesta: 'Correcto' });
     } else {
