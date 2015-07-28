5e97e9221d1ef45ad0c5febbb6f9ec767a326673
Jesús Ángel Pérez-Roca Fernández <djalma8@gmail.com>
2015-07-28 23:46:08 +0200

    Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,28 +1,36 @@
 var models = require('../models/models.js');
 
+// Autoload - factoriza el código si ruta incluye :quizId
+exports.load = function(req, res, next, quizId) {
+  models.Quiz.find(quizId).then(
+    function(quiz) {
+      if (quiz) {
+        req.quiz = quiz;
+        next();
+      } else { next(new Error('No existe quizId=' + quizId)); }
+    }
+  ).catch(function(error) { next(error);});
+};
+
 // GET /quizzes
 exports.index = function(req, res) {
-  models.Quiz.findAll().then(function(quizzes) {
-    res.render('quizzes/index.ejs', { quizzes: quizzes});
-  })
+  models.Quiz.findAll().then(
+    function(quizzes) {
+      res.render('quizzes/index', { quizzes: quizzes});
+    }
+  ).catch(function(error) { next(error);})
 };
 
 // GET /quizzes/:id
 exports.show = function(req, res) {
-  models.Quiz.find(req.params.quizId).then(function(quiz) {
-    res.render('quizzes/show', { quiz: quiz});
-  })
+  res.render('quizzes/show', { quiz: req.quiz});
 };
 
 // GET /quizzes/:id/answer
 exports.answer = function(req, res) {
-  models.Quiz.find(req.params.quizId).then(function(quiz) {
-    if (req.query.respuesta === quiz.respuesta) {
-      res.render('quizzes/answer', 
-                 { quiz: quiz, respuesta: 'Correcto' });
-    } else {
-      res.render('quizzes/answer', 
-                 { quiz: quiz, respuesta: 'Incorrecto'});
-    }
-  })
+  var resultado = 'Incorrecto';
+  if (req.query.respuesta === req.quiz.respuesta) {
+    resultado = 'Correcto';
+  }
+  res.render('quizzes/answer', {quiz: req.quiz, respuesta: resultado});
 };
--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,20 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+// Página principal
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
+
+ // Definición de rutas de /quizzes
 router.get('/quizzes',                      quizController.index);
 router.get('/quizzes/:quizId(\\d+)',        quizController.show);
 router.get('/quizzes/:quizId(\\d+)/answer', quizController.answer);
 
+// Página de autor
 router.get('/author', function(req, res, next) {
   res.render('author', { name: 'Jesús Ángel Pérez-Roca Fernández', photo: '../images/author.png' });
 });
